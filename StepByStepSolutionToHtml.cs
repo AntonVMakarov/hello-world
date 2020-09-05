51338704497d9538fe4edc2c90eab43e9ce7b2a1
AntonVMakarov <67121695+AntonVMakarov@users.noreply.github.com>
2020-09-05 20:25:25 +0300

    Update StepByStepSolutionToHtml.cs

--- StepByStepSolutionToHtml.cs
+++ StepByStepSolutionToHtml.cs
@@ -2,6 +2,11 @@ public class StepByStepSolutionToHtml
 {
    public string ToHtml(StepByStepSolution sbs)
    {
-       
+       // будем обходить дерево снизу =>
+       // "забуряемся" вниз дерева:
+       for(int i=0; i < sbs.ChildNodes.Count; i++)
+       {
+           
+       }
    }
 }
